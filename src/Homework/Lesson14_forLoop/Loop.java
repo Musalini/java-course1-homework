7b2b52719789357cea52afe8a3ac817941577dd5
Musa Muradzade <muradzade.musa@gmail.com>
2022-06-01 19:27:26 +0300

    add Lesson14_forLoop

---
+++ src/Homework/Lesson14_forLoop/Loop.java
@@ -0,0 +1,26 @@
+package Homework.Lesson14_forLoop;
+
+public class Loop {
+    public static void main(String[] args) {
+        showTime();
+    }
+
+    static void showTime() {
+        OUTER:
+        for (int hour = 0; hour < 6; hour++) {
+            MIDDLE:
+            for (int minutes = 0; minutes < 60; minutes++) {
+                if (hour > 1 && minutes % 10 == 0) {
+                    break OUTER;
+                }
+                INNER:
+                for (int seconds = 0; seconds < 60; seconds++) {
+                    if (hour * seconds > minutes) {
+                        continue MIDDLE;
+                    }
+                    System.out.println(hour + ":" + minutes + ":" + seconds);
+                }
+            }
+        }
+    }
+}
